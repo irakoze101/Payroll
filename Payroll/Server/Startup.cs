02ed861dbb6153b5000ec717b175cfde56790d48
Stefan Wert <stefan@anglr.tech>
2020-05-15 00:50:11 -0400

    Don't require account confirmation

--- Payroll/Server/Startup.cs
+++ Payroll/Server/Startup.cs
@@ -28,7 +28,7 @@ namespace Payroll.Server
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()

69d068e219b68bfc6139bace51b7a46ee26d766f
Stefan Wert <stefan@anglr.tech>
2020-05-18 13:47:40 -0400

    PayrollController test skeleton

---
+++ Payroll.Tests/TestPayrollController.cs
@@ -0,0 +1,8 @@
+﻿namespace Payroll.Tests
+{
+    public class TestPayrollController
+    {
+        // TODO: find a way to mock out GetUserId, which currently depends on ControllerBase implementation
+        // TODO: test benefits summary
+    }
+}
